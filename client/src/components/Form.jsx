29402282cab2d6a5a995bf420ed7f4ec1150c18b
“hxc338” <hxc338@student.bham.ac.uk>
2025-02-02 00:32:26 +0000

    completed handleSubmit function

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react"
+
 export default function Form () {
     // We will store the form values in state 
+    const [formValues, setFormValues] = useState(
+        {
+            username: "",
+            category: "",
+            recipeName: "",
+            time: "",
+            ingredients: "",
+            instructions: ""
+        }
+    )
+
     // We need submit handler 
     // In the submit handler, we have different tasks:
     // - We need to prevent the default behaviour of our form
     // - We need to fetch the POST route from the server (you don't need useEffect for this task)
+    function handleSubmit (event){
+        event.preventDefault();
+        alert("Form Submitted!")
+        //the client is trying to send a POST request to the /new-data route on the backend server 
+        fetch("http://localhost:8080/add-recipe", { 
+            method: "POST",
+            headers: {"Content-type": "application/json",},
+            body: JSON.stringify({formValues})
+        });
+    }
     
     // We need change handler 
+    
     return (
         <>
             <h2>Form</h2>
             {/* Write your form tags and all its elements in here, including the event listener (and the value attribute...) */}
-            <form>
+            <form onClick={handleSubmit}>
                 <label htmlFor="">Username:</label> <br/>
                 <input 
                     type="text" 
@@ -33,8 +57,8 @@ export default function Form () {
                 <label htmlFor="">Recipe Name:</label> <br/>
                 <input 
                     type="text" 
-                    name="recipe-name" 
-                    id="recipe-name"
+                    name="recipeName" 
+                    id="recipeName"
                     placeholder="Enter your recipe name"
                     required 
                 />
